7396720ca97baf00a71206a8620fdc35bae645b4
YONGHUAI <1039724903@qq.com>
2017-07-31 16:16:39 +0800

    第一次上传GitHub，第一版本

---
+++ qyhKeyBoardManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qyhKeyBoardManager
+//
+//  Created by Qiu on 16/4/3.
+//  Copyright © 2016年 YongHuaiQIu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
