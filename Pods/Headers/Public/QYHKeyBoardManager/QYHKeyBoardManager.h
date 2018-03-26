0c49a35d0082da8f32e4b008daf579c2e5975800
qiuyh@hori-gz.com <1039724903@qq.com>
2018-03-26 17:01:39 +0800

    删除

--- Pods/Headers/Public/QYHKeyBoardManager/QYHKeyBoardManager.h
+++
@@ -1,1 +0,0 @@
-../../../QYHKeyBoardManager/Classes/QYHKeyBoardManager.h
--- Pods/Target Support Files/Pods-qyhKeyBoardManager/Pods-qyhKeyBoardManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_qyhKeyBoardManager : NSObject
-@end
-@implementation PodsDummy_Pods_qyhKeyBoardManager
-@end
